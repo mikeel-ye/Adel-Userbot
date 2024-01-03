a69b352ecf629acc64ed69c12977c24cdde8fc18
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2024-01-03 11:12:09 +0700

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -31,7 +31,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning Asisstant-Key ${BRANCH}... "
+    echo -e "Cloning reza-userbot ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
