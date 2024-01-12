962bf9eb8c3f0a9fa19be88f85a93ae21d1f67f6
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2024-01-12 18:46:13 +0700

    update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -185,7 +185,7 @@ async def _(event):
     x = await event.eor("Ping !")
     end = round((time.time() - start) * 1000)
     uptime = time_formatter((time.time() - start_time) * 1000)
-    await x.edit(f"**ᴘᴏɴɢ !!** `{end}ᴍs`\n**ᴜᴘᴛɪᴍᴇ** - `{uptime}\nʀᴇᴢᴀ-ᴜsᴇʀʙᴏᴛ`\nᴏᴡɴᴇʀ {x.first_name}")
+    await x.edit(f"**ᴘᴏɴɢ !!** `{end}ᴍs`\n**ᴜᴘᴛɪᴍᴇ** - `{uptime}\nʀᴇᴢᴀ-ᴜsᴇʀʙᴏᴛ`\nᴏᴡɴᴇʀ `{x.first_name}")
 
 
 async def get_readable_time(seconds: int) -> str:
