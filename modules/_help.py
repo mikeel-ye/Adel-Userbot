e67c6d5c9ec74cfaff45588070b0cc865a205067
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2024-04-19 02:33:03 +0700

    Update _help.py

--- modules/_help.py
+++ modules/_help.py
@@ -31,7 +31,7 @@ async def _help(ayra):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @rezadgank"
+                output += "\n© @reza"
                 await ayra.eor(output)
             else:
                 try:
@@ -70,7 +70,7 @@ async def _help(ayra):
                     if file in HELP["Official"]:
                         for i in HELP["Official"][file]:
                             output += i
-                    output += "\n© @rezadgank"
+                    output += "\n© @reza"
                     await ayra.eor(output)
         except BaseException as er:
             LOGS.exception(er)
