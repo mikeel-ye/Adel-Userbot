ed709c669f3e7e9370358ce9ee7064139f9291bb
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-05-05 14:54:42 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -31,7 +31,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning reza-userbot ${BRANCH}... "
+    echo -e "Cloning Adel-Userbot ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
