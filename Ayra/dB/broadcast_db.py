d474d4207451d409a47354e50a881e183b412dfc
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-05-14 23:30:33 +0700

    Update broadcast_db.py

--- Ayra/dB/broadcast_db.py
+++ Ayra/dB/broadcast_db.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
-from .. import udB
+from .. import udB, varsdb
 
 
 def get_channels():  # Returns List
